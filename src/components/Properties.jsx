176196d3543703dc46d53cbbc69c32d99e24a28d
Sandyg77 <163460477+Sandyg77@users.noreply.github.com>
2025-01-01 15:16:52 +0530

    Added the json data and the correct path of the images and rendered json data to the properties section

--- src/components/Properties.jsx
+++ src/components/Properties.jsx
@@ -1,11 +1,40 @@
 import { Card } from "react-bootstrap";
+import propertyData from "../data/properties.json";
 
 const Properties = () => {
+  // Accessing the properties array from the JSON data
+  const properties = propertyData.properties;
+
   return (
     <Card style={{ width: "50rem" }}>
       <Card.Header>Available Properties 🏡</Card.Header>
       <Card.Body>
-        This is my Properties Component and where the json data should be loaded
+        <br />
+        {/* Checks whether data is available before rendering - && */}
+        {properties &&
+          properties.map((property) => {
+            return (
+              <div className="box" key={property.id}>
+                <p>
+                  <br />
+                  <strong>Type: {property.type}</strong>
+                </p>
+                <p>Price: ${property.price}</p>
+                <p>Location: {property.location}</p>
+                <p>
+                  Added: {property.added.day} {property.added.month}{" "}
+                  {property.added.year}
+                </p>
+                <img
+                  src={property.picture}
+                  alt={`Property ${property.id}`}
+                  className="img-fluid"
+                />{" "}
+                {/* bootstrap class used to make image responsivw */}
+                <button className="btn btn-dark">Add to Favourites</button>
+              </div>
+            );
+          })}
       </Card.Body>
     </Card>
   );
